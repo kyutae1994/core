6a8b3b3c2b371b5d27321a1e9fbdb260d1d7a063
kyutae <k305059ute@gmail.com>
2021-12-19 22:41:02 +0900

    스프링 공부 주문 회원 테스트

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
+
     private int discountFixAmount = 1000; // 1000원 할인
 
     @Override
